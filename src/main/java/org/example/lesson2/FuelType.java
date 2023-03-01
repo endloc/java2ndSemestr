a984239795bd3df673d14072142a3de33bb2b7b5
endloc <li.amirova.l@gmail.com>
2023-03-01 23:30:18 +0300

    constructors & Scanner

--- src/main/java/org/example/lesson2/FuelType.java
+++
@@ -1,5 +0,0 @@
-package org.example.lesson2;
-
-public enum FuelType {
-    FUEL_95, FUEL_95_GDRIVE, FUEL_98
-}
--- src/main/java/org/example/lesson2/Num2.java
+++
@@ -1,6 +0,0 @@
-package org.example.lesson2;
-
-// призма:
-public class Num2 {
-    
-}
